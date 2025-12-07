d5ff992eca06baa49c89fb46ee3775493ea2139d
Bill Evans <not@notsteve.net>
2025-12-06 21:22:07 -0500

    Update be-schema-engine.php

--- be-schema-engine/be-schema-engine.php
+++ be-schema-engine/be-schema-engine.php
@@ -1,1 +1,21 @@
-<?php echo "Hello World"; ?>
+/**
+ * -------------------------------------------------------------------------
+ * Homepage schema
+ * -------------------------------------------------------------------------
+ *
+ * Conditions:
+ * - Global schema enabled (be_schema_globally_disabled() === false)
+ * - is_front_page()
+ * - Not disabled for current page (be_schema_is_disabled_for_current_page() === false)
+ *
+ * Output:
+ * - @graph of:
+ *   - Person (if enabled)
+ *   - Person image (if available)
+ *   - Organisation (if enabled)
+ *   - WebSite
+ *   - logo
+ *   - Publisher (if enabled)
+ *   - Publisher logo (if enabled)
+ *   - WebPage for the homepage.
+ */
